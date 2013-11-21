38bdfbfc12a76e7193a685312e07d08f50469cd7
Ralph Caraveo <rcaraveo@loopnet.com>
2013-11-20 18:12:34 -0800

    Now have the data for grid loading from json

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"log"
 	"math"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
@@ -42,8 +44,17 @@ var pool = &redis.Pool{
 	},
 }
 
+type State struct {
+	Dungeon []string
+}
+
 func main() {
 	defer un(trace("main()"))
+
+	/*
+		REMEMBER: ULTIMATE GOAL
+		ABSOLUTELY NO STATE IN WEBSERVER...should all be in REDIS!!!!!!!
+	*/
 
 	TestConnection()
 	ClearAllState()
@@ -56,6 +67,13 @@ func main() {
 
 	dungeonBoard := DungeonBytesToBoard(paddedDungeon)
 	PrintBoard(dungeonBoard)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, GetDungeonJSON())
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func trace(s string) (string, time.Time) {
@@ -69,7 +87,10 @@ func un(s string, startTime time.Time) {
 }
 
 func TestConnection() {
+
 	conn := pool.Get()
+	defer conn.Close()
+
 	x, err := redis.String(conn.Do("SET", "NAME", "RALPH"))
 	if err != nil {
 		log.Println("Perhaps Redis is offline or somethin'")
@@ -83,12 +104,15 @@ func ClearAllState() {
 	log.Println("ClearAllState()")
 
 	conn := pool.Get()
+	defer conn.Close()
+
 	conn.Send("MULTI")
 	conn.Send("DEL", "DUNGEON")
 	_, err := conn.Do("EXEC")
 	if err != nil {
 		log.Fatal("Couldn't clear all state")
 	}
+
 }
 
 func AddRoom(offset int) {
@@ -114,6 +138,8 @@ func GetDungeon() []byte {
 	log.Println("GetDungeon()")
 
 	conn := pool.Get()
+	defer conn.Close()
+
 	repl, err := redis.Bytes(conn.Do("GETRANGE", "DUNGEON", 0, BOARDSIZE))
 	if err != nil {
 		log.Fatal(err)
@@ -166,4 +192,22 @@ func PrintBoard(dungeon string) {
 	result = result[0:BOARDSIZE]
 
 	fmt.Println("[" + strings.Join(result, ",") + "]")
+}
+
+func GetDungeonJSON() string {
+	b := GetDungeon()
+	pd := PadDungeon(b)
+	dungeon := DungeonBytesToBoard(pd)
+	result := strings.Split(dungeon, "")
+
+	//truncate to max size of dungeon because of extra padding of bits remainder of byte at the end
+	result = result[0:BOARDSIZE]
+
+	s := State{Dungeon: result}
+	b, err := json.Marshal(s)
+
+	if err != nil {
+		log.Fatal("Could not get Dungeon JSON")
+	}
+	return string(b)
 }
